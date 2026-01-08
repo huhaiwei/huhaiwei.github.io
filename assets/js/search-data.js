f14a805082aea77f1f9054da97b785ba14723004
huhaiwei <112732024+huhaiwei@users.noreply.github.com>
2026-01-08 06:46:26 +0000

    Deploying to gh-pages from @ huhaiwei/huhaiwei.github.io@13d6fed6baf7471a9405b97733c7aea3f586eb35 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-cv",
-          title: "CV",
+          title: "CV 😸",
           description: "",
           section: "Navigation",
           handler: () => {
